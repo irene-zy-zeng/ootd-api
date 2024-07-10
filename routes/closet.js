1ca4e9f80d92cc3e11c96e6904aa7f7facd6e57e
Irene Tsang <tsangchiyan@Irenes-MacBook-Pro.local>
2024-07-10 16:47:52 -0400

    Set up endpoint to get all items

--- routes/closet.js
+++ routes/closet.js
@@ -1,5 +1,35 @@
 import express from "express";
+import fs from "fs";
 
 const router = express.Router();
+
+const readCloset = () => {
+  const closetFile = fs.readFileSync("./data/closet.json");
+  const items = JSON.parse(closetFile);
+  return items;
+};
+
+//Get all items
+router.get("/closet", (_req, res) => {
+  try {
+    const itemsData = readCloset();
+    // console.log(itemsData);
+    const dataServerd = itemsData.map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        image: item.image,
+        category: item.category,
+        color: item.color,
+        season: item.season,
+        brand: item.brand
+      };
+    });
+    res.json(dataServerd);
+  } catch (error) {
+    console.error("Error reading albums file", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 export default router;
